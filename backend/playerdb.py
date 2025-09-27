8036cc58c8ef10384e5b0fb778109f4003e81610
CQVan <chandlerqvan@gmail.com>
2025-09-27 12:37:02 -0700

    implemented playerdb and questiondb

--- backend/playerdb.py
+++ backend/playerdb.py
@@ -1,14 +1,40 @@
+import boto3
+from dataclasses import dataclass
 
+# Updated Player class
+@dataclass
 class Player:
-    name: str
-    uuid: str
+    id: str
     elo: int
 
-def get_player() -> Player:
-    return Player("test", "uuid-here", 400)
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('Players')
+
+def get_player(player_id: str) -> Player | None:
+    response = table.get_item(Key={'id': player_id})
+    item = response.get('Item')
+
+    if not item:
+        return None
+
+    return Player(
+        id=item['id'],
+        elo=item['elo']
+    )
 
 def create_player(player: Player):
-    return
+    table.put_item(
+        Item={
+            'id': player.id,
+            'elo': player.elo
+        }
+    )
 
 def update_player(player: Player):
-    return
+    table.update_item(
+        Key={'id': player.id},
+        UpdateExpression="SET elo = :elo",
+        ExpressionAttributeValues={
+            ':elo': player.elo
+        }
+    )
