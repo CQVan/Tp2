bf34a6d23e4dcc873f8b3b0228d0d62eaebf70fc
CQVan <chandlerqvan@gmail.com>
2025-09-27 21:39:15 -0700

    added dockerfile and framework for testing

---
+++ frontend/src/compilers/compiler.ts
@@ -0,0 +1,16 @@
+abstract class Compiler {
+    /**
+     * Abstract method to run code.
+     * @param code - The code to execute.
+     * @param args - Optional arguments to pass to the code.
+     * @returns An object of type `any`.
+     */
+    abstract run(code: string, args?: any[]): any;
+}
+
+class JavaScript extends Compiler{
+    run(code: string, args?: any[]) {
+        
+    }
+    
+}
